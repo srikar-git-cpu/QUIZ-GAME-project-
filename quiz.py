40e7dce7eddb4afa3d67269d26c17a8d705b2d72
srikar-git-cpu <nagubandisrikar73@gmail.com>
2026-06-15 23:41:54 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -14,6 +14,6 @@ else:
 
 print('question3: what is national tree')
 answer3 = input("your answer: ")
-if answer3.lower == "banyan tree"
+if answer3.lower == "banyan tree" :
 else : 
     print("wrong")
