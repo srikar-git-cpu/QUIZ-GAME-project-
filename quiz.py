147d8d078d5d1d49e88aefa6911aec4b5a122eb4
srikar-git-cpu <nagubandisrikar73@gmail.com>
2026-06-15 23:41:25 +0530

    Update and rename app.py to quiz.py
    for this i spent my time with claude

---
+++ quiz.py
@@ -0,0 +1,19 @@
+print("question1: what is capital of india")
+answer2 = input("your answer: ")
+if answer2.lower() == "new delhi" :
+    print("correct")
+else :
+    print("wrong")
+
+print("question2: what is national bird")
+answer2 = input("your answer: ")
+if answer2.lower() == "peacock" :
+    print("correct")
+else:
+    print("wrong")
+
+print('question3: what is national tree')
+answer3 = input("your answer: ")
+if answer3.lower == "banyan tree"
+else : 
+    print("wrong")
