313ae2d7ad90e84f0c1ce9a4e7e8aea0f2092b88
Nagubandi srikar <nagubandisrikar73@gmail.com>
2026-06-16 00:05:58 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 print("question1: what is capital of india")
-answer2 = input("your answer: ")
-if answer2.lower() == "new delhi" :
+answer1 = input("your answer: ")
+if answer1.lower() == "new delhi" :
     print("correct")
 else :
     print("wrong")
@@ -14,6 +14,6 @@ else:
 
 print('question3: what is national tree')
 answer3 = input("your answer: ")
-if answer3.lower == "banyan tree" :
+if answer3.lower() == "banyan tree" :
 else : 
     print("wrong")
