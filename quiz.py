fdd755ac777d2dd03f3338942092a53ff19cc32b
Nagubandi srikar <nagubandisrikar73@gmail.com>
2026-06-17 12:18:35 +0530

    Update quiz.py
    To this i added score system

--- quiz.py
+++ quiz.py
@@ -1,14 +1,18 @@
+score = 0
+
 print("question1: what is capital of india")
 answer1 = input("your answer: ")
 if answer1.lower() == "new delhi" :
     print("correct")
+    score += 1
 else :
     print("wrong")
-
+    
 print("question2: what is national bird")
 answer2 = input("your answer: ")
 if answer2.lower() == "peacock" :
     print("correct")
+    score += 1
 else:
     print("wrong")
 
@@ -16,5 +20,10 @@ print('question3: what is national tree')
 answer3 = input("your answer: ")
 if answer3.lower() == "banyan tree" :
     print("correct")
+    score += 1
 else : 
     print("wrong")
+
+print("your score is" , score , "out of 3")
+
+
